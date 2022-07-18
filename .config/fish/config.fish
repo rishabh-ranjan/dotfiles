634958c92ba253ac7f43548c49584c4433f89874
Rishabh Ranjan <mr.rishabh.ranjan@gmail.com>
2022-07-18 22:49:38 +0530

    fisher install anyway doesn't work without setting up proxy which happens only later when custom.fish is sourced

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,9 @@ if status is-interactive
 	set -x LC_ALL C.UTF-8
 
 	if not functions --query tide
-		fisher install IlanCosman/tide@v5
-		echo 1 1 3 2 2 1 1 1 y | tide configure > /dev/null
+		echo '=== setup prompt ===
+			fisher install IlanCosman/tide@v5
+			echo 1 1 3 2 2 1 1 1 y | tide configure > /dev/null'
 	end
 
 	fish_vi_key_bindings
