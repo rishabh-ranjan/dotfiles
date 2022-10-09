784f77648762c97dbbce60074b33a82f96f008ec
Rishabh Ranjan <mr.rishabh.ranjan@gmail.com>
2022-10-10 01:59:57 +0530

    add tabular

--- .config/fish/custom/iitd.fish
+++ .config/fish/custom/iitd.fish
@@ -4,6 +4,8 @@ if status is-interactive
 	set -x https_proxy "$ip"
 
 	set -x GRB_LICENSE_FILE "$HOME/.config/gurobi/$hostname.lic"
+
+	set -x XLA_FLAGS --xla_gpu_force_compilation_parallelism=1
 	# conda activate ilploss
 	conda activate grb
 end
