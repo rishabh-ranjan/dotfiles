6df9cefc017ca3a8294885b97e2a1e38e776c295
Rishabh Ranjan <mr.rishabh.ranjan@gmail.com>
2022-10-13 14:03:09 -0400

    refix locale

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,4 @@
 if status is-interactive
-	# set -x LANG en_US.UTF-8
-	# set -x LANGUAGE en_US.UTF-8
-	# set -x LC_ALL en_US.UTF-8
-	# set -x LC_CTYPE UTF-8
-
 	set -x EDITOR nvim
 	set -x VISUAL nvim
 
