c20bfa132c95ecf518a98c07f2e810e2a0ff07b5
Rishabh Ranjan <rishabhranjan0207@gmail.com>
2023-04-23 10:34:30 -0400

    locale

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
 if status is-interactive
 	set -x EDITOR nvim
 	set -x VISUAL nvim
-	set -x LANG en_US.utf8
-	set -x LANGUAGE en_US.utf8
-	set -x LC_ALL en_US.utf8
+	set -x LANG en_US.UTF-8
+	set -x LANGUAGE en_US.UTF-8
+	set -x LC_ALL en_US.UTF-8
 
 	if not functions --query tide
 		echo '=== setup prompt ===
