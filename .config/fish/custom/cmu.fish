765b6020afaa00ce44b7297902a299231d7e9952
rishabh ranjan <rishabhranjan0207@gmail.com>
2023-06-07 21:49:41 +0000

    excube and node (for copilot)

--- .config/fish/custom/cmu.fish
+++ .config/fish/custom/cmu.fish
@@ -4,8 +4,7 @@ if status is-interactive
 	set -x https_proxy "$ip"
 	set -x HTTP_PROXY "$ip"
 	set -x HTTPS_PROXY "$ip"
-	set -x HYDRA_FULL_ERROR "1"
 	set -x WANDB_SILENT "true"
 	set -x WANDB_CONSOLE "off"
-	conda activate nsd
+	conda activate nsd-expts
 end
